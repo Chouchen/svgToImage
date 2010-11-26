3929b47d7616cb20866c9c3b89ea7ddb0e99dc15
Chouchen <Chouchen@8df70b78-0add-44c1-aa98-c5855ad9a2c0>
2010-11-26 17:18:17 +0000

    Commencement de parsing Path (c'est le bordel entre le W3C et l'utilisation réelle...) Ajout des couleurs écrites (black, etc.)

--- trunk/test.php
+++ trunk/test.php
@@ -9,9 +9,10 @@ $svg = '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="600" height
 <defs></defs>
 <image x="0" y="0" width="300" height="512" preserveAspectRatio="none" href="http://labs.shikiryu.com/experimental-cut/images/pieces/fond.jpg"></image>
 <rect x="168" y="275" width="52" height="70" r="0" rx="0" ry="0" fill="none" stroke="#FFF" stroke-width="3" stroke-dasharray="8,3" transform="rotate(21.91207728 194 310)" style="opacity: 1;" opacity="1"></rect>
-<circle cx="50" cy="50" r="50" fill="#FFFFFF" stroke="#000"></circle>
+<circle cx="50" cy="50" r="50" fill="turquoise" stroke="#000"></circle>
 <circle cx="100" cy="50" r="40" stroke="#000" stroke-width="2" fill="none"/>
 <image x="170" y="277" width="48" height="66" preserveAspectRatio="none" href="http://labs.shikiryu.com/experimental-cut/images/pieces/1.png" style="cursor: move; opacity: 1; " r="90" opacity="1" transform="rotate(21.91207728 194 310)"></image>
+<path d="M250 150 L150 350 L350 350 Z" />
 </svg>';
 
 $svgtoimage = SVGTOIMAGE::parse($svg);
